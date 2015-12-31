560b3512b525f9ca8b1b14a330ae869c6d9cd1d1
Gordie Chown <gordie.chown@gmail.com>
2015-12-31 12:51:48 -0500

    Add send rating, state saving, animation

--- app/src/main/java/com/example/gordie/howstheg/Connection.java
+++ app/src/main/java/com/example/gordie/howstheg/Connection.java
@@ -1,36 +1,40 @@
 package com.example.gordie.howstheg;
 
 import android.os.AsyncTask;
-import android.widget.RatingBar;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
 
 /**
  * Created by gordie on 08/12/15.
- * Used to receive and send information to server - at time of writing not yet implemented.
+ * Used to receive and send information to server.
  */
 public class Connection {
-    Socket client;
-    DataInputStream din;
-    DataOutputStream dout;
+    private Socket client;
+    private DataInputStream din;
+    private DataOutputStream dout;
     private float gotRating;
-    String host;
-    int port;
+    private String host;
+    private String android_id;
+    private int port;
+    private boolean isConnected;
+    private int numVotes;
     /**
      * Creates connection to host
      * @param host IP of host to connect to
      * @param port Port of host to connect to
      */
-    public Connection(String host, int port){
+    public Connection(String host, int port, String android_id){
         this.host = host;
         this.port = port;
+        this.android_id = android_id;
+        isConnected = false;
         gotRating = 0;
+        numVotes = 0;
         try {
-            gotRating = new fetchRating().execute(host, "" + port).get();
+            new connect().execute(host, "" + port, android_id);
+            gotRating = new fetchRating().execute().get();
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -41,8 +45,11 @@ public class Connection {
      * @param score Number of stars (out of five) to be sent
      */
     public void sendRating(int score){
-        if(score < 0 || score > 5){
+        if(score < -1 || score > 5){
             throw new IllegalArgumentException("Score must be 0 < x < 5!");
+        }else{
+            if(score == 0) score = 1;
+            new sendRating().execute(score);
         }
     }
 
@@ -53,13 +60,6 @@ public class Connection {
     public float getRating(){
         return gotRating;
     }
-    public void setRating(RatingBar bar) {
-        try{
-            bar.setRating(new fetchRating().execute().get());
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Gets number of people who have voted
@@ -67,27 +67,69 @@ public class Connection {
      */
     public int getNumVotes(){
 
-        return 10;
+        return numVotes;
     }
 
+    /**
+     * Tells if the socket is connected to the server
+     * @return true if connected to server
+     */
+    public boolean isConnected(){
+        return isConnected;
+    }
 
-    public class fetchRating extends AsyncTask<String, Void, Float> {
-        protected Float doInBackground(String... params){
-            System.out.println("Running in background");
-            float returning = 1;
+    public class connect extends AsyncTask<String, Void, Void>{
+        protected Void doInBackground(String... params){
             try{
-                client = new Socket(host, port);
+                System.out.println("Connecting to server");
+                client = new Socket(params[0], Integer.parseInt(params[1]));
                 dout = new DataOutputStream(client.getOutputStream());
                 din = new DataInputStream(client.getInputStream());
-                dout.writeInt(0);
-                byte[] recv = new byte[2];
-                din.read(recv);
-                returning = recv[0];
-                gotRating = (int) returning;
+                dout.writeUTF(params[2]);
+                System.out.println("Sending device ID " + params[2]);
+                if(client.isConnected()) isConnected = true;
             }catch(IOException e){
                 e.printStackTrace();
             }
-            return returning;
+            return null;
+        }
+    }
+
+    /**
+     * Connects to server, gets user rating and numVotes
+     */
+    public class fetchRating extends AsyncTask<Void, Void, Float> {
+
+        protected Float doInBackground(Void... params){
+            try{
+                if(client != null && dout != null && din != null) {
+                    dout.writeInt(1);
+                    gotRating = din.readFloat();
+                    numVotes = din.readInt();
+                    System.out.println("Got rating " + gotRating + ", numVotes " + numVotes);
+                    return gotRating;
+                }
+            }catch(IOException e){
+                isConnected = false;
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public class sendRating extends AsyncTask<Integer, Void, Void>{
+        protected Void doInBackground(Integer... params){
+            try{
+                if(client != null && dout != null && din != null) {
+                    dout.writeInt(0);
+                    dout.writeInt(params[0]);
+                    System.out.println("Sent " + params[0]);
+                }
+            }catch(IOException e){
+                isConnected = false;
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
--- app/src/main/java/com/example/gordie/howstheg/MainActivity.java
+++ app/src/main/java/com/example/gordie/howstheg/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gordie.howstheg;
 
+import android.animation.ObjectAnimator;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RatingBar;
@@ -7,28 +9,77 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
 
 public class MainActivity extends AppCompatActivity {
-    RatingBar otherRating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         final String HOST = "192.168.1.127";
         final int PORT = 8000;
-        otherRating = (RatingBar) findViewById(R.id.otherRatingsBar);
-        Connection connection = new Connection(HOST, PORT);
-        otherRating.setRating(connection.getRating());
+        String android_id = Settings.Secure.getString(getContentResolver(),Settings.Secure.ANDROID_ID);
+        final Connection connection = new Connection(HOST, PORT, android_id);
 
-        //TODO: send userRating to server
+        RatingBar otherRating = (RatingBar) findViewById(R.id.otherRatingsBar);
         RatingBar userRating = (RatingBar) findViewById(R.id.ratingBar);
+
+
+        if (savedInstanceState != null) {
+            // Restore value of members from saved state
+            otherRating.setRating(savedInstanceState.getFloat("otherRating"));
+            userRating.setRating(savedInstanceState.getFloat("myRating"));
+        } else {
+            setRating(otherRating, connection);
+        }
+
+
+        //Send rating on change
+        userRating.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                connection.sendRating((int) rating);
+            }
+        });
+
+        setLabels();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putFloat("otherRating", ((RatingBar) findViewById(R.id.otherRatingsBar)).getRating());
+        savedInstanceState.putFloat("userRating", ((RatingBar) findViewById(R.id.ratingBar)).getRating());
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        setRating(((RatingBar)findViewById(R.id.ratingBar)), savedInstanceState.getFloat("userRating"));
+        setRating(((RatingBar)findViewById(R.id.otherRatingsBar)), savedInstanceState.getFloat("otherRating"));
+    }
+
+    private void setRating(RatingBar bar, float rating){
+        ObjectAnimator anim = ObjectAnimator.ofFloat(bar, "rating", rating);
+        anim.setDuration(1200);
+        anim.start();
+    }
+    private void setRating(RatingBar bar, Connection connection) {
+        setRating(bar, connection.getRating());
+    }
+
+    //Very broken at the moment.
+    private void setLabels(){
         //Sets meal text
         DateFormat dateFormat = new SimpleDateFormat("HHmm");
         Calendar cal = Calendar.getInstance();
         int time = Integer.parseInt(dateFormat.format(cal.getTime()));
         //time = 2000; //testing code, remove in release
         String meal = getMeal(time);
-
         String entering = "It's ";
         if(time < 8000){
             entering = "Caf opens at 8 tomorrow.";
@@ -44,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         }
         ((TextView) findViewById(R.id.mealDescrip)).setText(entering);
     }
+
     /**
      * Returns breakfast (hour of day < 1000 (10AM)),
      * lunch (1000 < hour of day < 1500 (3PM)), or dinner (1500 < hour of day)
@@ -57,7 +109,6 @@ public class MainActivity extends AppCompatActivity {
         if(time > 4)
             return "dinner";
         else return " ...I'm not actually sure!";
-
     }
 }
 
